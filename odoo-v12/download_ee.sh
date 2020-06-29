e7f11ec5941ee94385bc47f54a06bb0079c86051
Carlitos Melgar <carlitos.melgar@izote.net>
2020-06-28 22:33:29 -0400

    Made improvements: - unzip without logging all extracted files. - fixed bug of $token not being read properly. - added file token/token.txt to be read into the download_ee.sh script. This way only one token is isntalled and all versions will work

--- odoo-v12/download_ee.sh
+++ odoo-v12/download_ee.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-token="PERSONAL ACCESS TOKEN"
+token=$(<../token/token.txt)
 
 rm -r ../../odoo_latest_src/enterprise/v12
 
-curl -H 'Authorization: token $token' \
+curl -H "Authorization: token $token" \
   -H 'Accept: application/vnd.github.v4.raw' \
   -O \
   -L https://github.com/odoo/enterprise/archive/12.0.zip
 
-unzip 12.0.zip
+unzip -q 12.0.zip
 rm 12.0.zip
 mv enterprise-12.0 ../../odoo_latest_src/enterprise/v12
--- odoo-v13/download_ee.sh
+++ odoo-v13/download_ee.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-token="PERSONAL ACCESS TOKEN"
+token=$(<../token/token.txt)
 
 rm -r ../../odoo_latest_src/enterprise/v13
 
-curl -H 'Authorization: token $token' \
+curl -H "Authorization: token $token" \
   -H 'Accept: application/vnd.github.v4.raw' \
   -O \
   -L https://github.com/odoo/enterprise/archive/13.0.zip
 
-unzip 13.0.zip
+unzip -q 13.0.zip
 rm 13.0.zip
 mv enterprise-13.0 ../../odoo_latest_src/enterprise/v13
