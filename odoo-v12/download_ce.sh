1e4364363cffaf03a959294099e03936e4c03aa8
Carlitos Melgar <carlitos.melgar@izote.net>
2020-06-28 22:35:22 -0400

    Made improvements: - unzip without logging all extracted files. - rm will remove recursively

--- odoo-v12/download_ce.sh
+++ odoo-v12/download_ce.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -r ../../odoo_latest_src/community/v12
 curl  https://nightly.odoo.com/12.0/nightly/tgz/odoo_12.0.latest.zip --output odoo_12.0.latest.zip
-unzip odoo_12.0.latest.zip -d latest_src
+unzip -q odoo_12.0.latest.zip -d latest_src
 rm odoo_12.0.latest.zip
 mv latest_src/odoo-12.0* ../../odoo_latest_src/community/v12
-rm -d latest_src
+rm -r latest_src
