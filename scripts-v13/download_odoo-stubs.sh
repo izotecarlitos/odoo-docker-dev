82e795295926d0df0e47af31e6279e6e2a277a43
Carlitos Melgar <carlitos.melgar@izote.net>
2021-03-19 18:02:28 -0300

    moved odoo stubs location

--- scripts-v13/download_odoo-stubs.sh
+++ scripts-v13/download_odoo-stubs.sh
@@ -2,8 +2,12 @@
 scriptdir="$(dirname "$0")"
 cd "$scriptdir"
 
-rm -r ../../odoo-stubs/v13
+echo "Downloading odoo-stubs v13 \n"
+
+rm -r ../../odoo_latest_src/odoo-stubs/v13
+
 curl  https://codeload.github.com/trinhanhngoc/odoo-stubs/zip/13.0 --output odoo-stubs.zip
+
 unzip -q odoo-stubs.zip
 rm odoo-stubs.zip
-mv odoo-stubs* ../../odoo-stubs/v13
+mv odoo-stubs* ../../odoo_latest_src/odoo-stubs/v13
